130455413c3f785ce0950c0c9f26dda73237307c
dc <dyxxxyd@gmail.com>
2019-03-08 14:46:13 +0800

    add bubble_sort

--- algorithm/bubble_sort.php
+++ algorithm/bubble_sort.php
@@ -3,10 +3,11 @@
 function bubble_sort($arr)
 {
     $length = count($arr) - 1;
+    $tmp = 0;
     for ($i = 0; $i < $length; ++$i) {
-        for ($j = 0; $j <= $i - 1; ++$j) {
-            if ($arr[$j] > $arr[$j++]) {
-                $tmp = $arr[$j];
+        for ($j = 0; $j < $length - $i; ++$j) {
+            if ($arr[$j] > $arr[$j + 1]) {
+                $tmp = $arr[$j + 1];
                 $arr[$j + 1] = $arr[$j];
                 $arr[$j] = $tmp;
             }
