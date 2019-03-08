5853fc26882245d38ae5edb3b830c56d112e97d2
dc <dyxxxyd@gmail.com>
2019-03-08 18:04:07 +0800

    concept

--- algorithm/bubble_sort.php
+++ algorithm/bubble_sort.php
@@ -16,3 +16,28 @@ function bubble_sort($arr)
 
     return $arr;
 }
+
+// 优化思路：减少比较次数和交换次数
+
+function bubble_sort_v2($arr)
+{
+    $tmp = 0;
+    $length = count($arr);
+    for ($i = 0; $i < $length; ++$i) {
+        $flag = true;
+        for ($j = 0; $j < $length - 1; ++$j) {
+            if ($arr[$j] < $arr[$j + 1]) {
+                $tmp = $arr[$j + 1];
+                $arr[$j + 1] = $arr[$j];
+                $arr[$j] = $tmp;
+                $flag = false;
+            }
+        }
+        if ($flag) {
+            break;
+        }
+        echo 'loop i: '.$i.PHP_EOL;
+    }
+
+    return $arr;
+}
